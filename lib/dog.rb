856e97467394ab131c3c703d01dc1395a6251009
Robert Sainton <robertsainton@outlook.com>
2022-11-14 14:33:46 -0500

    commit tree 3e80309b72ff7fcb61dcf56df2a9551491d70270

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,24 @@
 class Dog
+    def name=(dog_name)
+        @name = dog_name
+    end
+    def name
+        @name
+        end
+        
+    def breed=(dog_breed)
+        @breed = dog_breed
+    end
+    def breed
+        @breed
+        end
 
 end
+
+snoopy = Dog.new
+snoopy.name = "snoopy"
+puts snoopy.name
+
+snoopy.breed = "pointer"
+
+puts snoopy.breed
